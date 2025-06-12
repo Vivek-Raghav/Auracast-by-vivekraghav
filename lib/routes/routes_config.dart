4ffbc2ccaa6115be57b2b7efb3e1b67f397d1271
Vivek-vertune <vivek@vertune.io>
2025-06-13 04:42:49 +0530

    CustomtransitionAdded

--- lib/routes/routes_config.dart
+++ lib/routes/routes_config.dart
@@ -1,16 +1,25 @@
 import 'package:auracast/routes/app_routes.dart';
 import 'package:auracast/routes/routes_index.dart';
+import 'package:auracast/routes/page_transitions.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: AppRoutes.home,
   routes: [
     GoRoute(
       path: AppRoutes.home,
-      builder: (context, state) => const HomePage(),
+      pageBuilder: (context, state) => customTransitionPage(
+        context: context,
+        state: state,
+        child: const HomePage(),
+      ),
     ),
     GoRoute(
       path: AppRoutes.citySearch,
-      builder: (context, state) => const CitySearchScreen(),
+      pageBuilder: (context, state) => customTransitionPage(
+        context: context,
+        state: state,
+        child: const CitySearchScreen(),
+      ),
     ),
   ],
 );
